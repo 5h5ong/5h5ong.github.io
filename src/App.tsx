773341e412c2f2db5b2a4c2874306d9aac12d252
call5h5ong <call5h5ong@gmail.com>
2021-03-30 12:19:52 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input placeholder="변환할 문장을 입력해주세요." />
+      <div>변환된 문자열 나올 곳</div>
     </div>
   );
 }
