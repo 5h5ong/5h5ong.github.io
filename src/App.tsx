30aa484a4e395f0b4637d67e51d144a038a0945f
call5h5ong <call5h5ong@gmail.com>
2021-04-02 01:52:35 +0900

    result-div 클릭 시 ripple element의 위치 계산 및 표시
    ripple effect의 기본이 되는 원 형태의 element가 클릭된 곳에 표시될 수 있게 만듬.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import useInput from "./hooks/useInput";
 import copyToClipboard from "./libs/copyToClipboard";
@@ -8,6 +8,10 @@ import separateKor from "./libs/separateKor";
 function App() {
   const korInput = useInput();
   const [convertedText, setConvertedText] = useState("");
+  // result div의 ref(ripple effect를 위해 사용됨)
+  const resultDivRef = useRef<HTMLDivElement>(null);
+  // result div 클릭 좌표 저장(ripple effect를 위해 사용됨)
+  const [coords, setCoords] = useState<{ x: number; y: number }>();
 
   // 입력된 한글 문장을 분리
   useEffect(() => {
@@ -20,6 +24,18 @@ function App() {
     copyToClipboard(convertedText);
   };
 
+  // result div의 좌표와 클릭된 좌표를 얻어 ripple effect가
+  // 생길 부분을 계산
+  const getCoords = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    // result div의 좌표(좌측 상단)
+    const rect =
+      resultDivRef.current && resultDivRef.current.getBoundingClientRect();
+    // ripple element가 표시될 좌표 계산
+    if (rect) {
+      setCoords({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+    }
+  };
+
   return (
     <div className="container">
       <div className="input-container">
@@ -30,8 +46,30 @@ function App() {
           onChange={korInput.onChange}
         />
       </div>
-      <div className="text-result" onClick={() => engTextOnClick()}>
+      <div
+        className="text-result"
+        ref={resultDivRef}
+        onClick={(e) => {
+          engTextOnClick();
+          getCoords(e);
+        }}
+      >
+        {coords && coords.x && (
+          <div
+            className="test-dot"
+            style={{
+              left: coords.x - 50,
+              top: coords.y - 50,
+            }}
+          />
+        )}
+
         {convertedText}
+      </div>
+      <div>
+        <div>click coords</div>
+        <div>{coords?.x}</div>
+        <div>{coords?.y}</div>
       </div>
     </div>
   );
